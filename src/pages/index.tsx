b92772df12f139044d7e727acf973cc9f51bb541
lets00 <eduardovansilva@gmail.com>
2022-06-04 13:06:50 -0300

    Adicionado componente Titulo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from "next";
+import Titulo from "../ui/components/Titulo/Titulo";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Titulo
+        titulo=""
+        subtitulo={
+          <span>
+            Com um pequeno valor mensal você pode{" "}
+            <strong>adotar um pet virtualmente</strong>
+          </span>
+        }
+      ></Titulo>
+    </div>
+  );
 };
 
 export default Home;
