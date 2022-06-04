c5315f00947b31e738ac37223677cb7ad896eba4
lets00 <eduardovansilva@gmail.com>
2022-06-04 12:39:13 -0300

    Adicionado tema

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
-import type { AppProps } from 'next/app'
-import '../ui/styles/globals.css'
+import type { AppProps } from "next/app";
+import "../ui/styles/globals.css";
+import { ThemeProvider } from "@mui/material";
+import tema from "../ui/themes/tema";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={tema}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
